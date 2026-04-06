eb81585e132d2753f5b79861bfdf3a7600aaaa5d
Alifian <alifiancandra93@gmail.com>
2026-04-07 03:52:05 +0700

    switch collor background

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 text-gray-200 py-6 mt-12">
+    <footer className="bg-[#020617] text-gray-200 py-6 mt-12">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-4">
         <p className="text-sm">&copy; 2026 Alifian. All rights reserved.</p>
         <div className="flex space-x-4 mt-2 md:mt-0">
