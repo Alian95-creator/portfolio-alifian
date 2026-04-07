0e3c05446deb287bdfe140782337355df6794e76
Alifian <alifiancandra93@gmail.com>
2026-04-07 11:03:22 +0700

    fix: remove structure icon

--- src/components/CustomIcons.jsx
+++ src/components/CustomIcons.jsx
@@ -1,23 +1,22 @@
 import React from "react";
+import htmlIcon from "../assets/icon/html.svg";
+import cssIcon from "../assets/icon/css.svg";
+import jsIcon from "../assets/icon/javascript.svg";
+import reactIcon from "../assets/icon/react.svg";
+import nodeIcon from "../assets/icon/nodejs.svg";
+import tailwindIcon from "../assets/icon/tailwind.svg";
 
-const icons = [
-  "/icon/html.svg",
-  "/icon/css.svg",
-  "/icon/javascript.svg",
-  "/icon/react.svg",
-  "/icon/nodejs.svg",
-  "/icon/tailwind.svg",
-];
+const icons = [htmlIcon, cssIcon, jsIcon, reactIcon, nodeIcon, tailwindIcon];
 
 export default function CustomIcons() {
   return (
-    <div className="flex gap-4 mt-4">
+    <div className="flex flex-wrap gap-4 mt-4">
       {icons.map((icon, idx) => (
         <img
           key={idx}
           src={icon}
           alt="tech icon"
-          className="w-8 h-8 cursor-pointer transform transition-transform duration-300 hover:scale-125"
+          className="w-12 h-12 sm:w-10 sm:h-10 cursor-pointer transform transition-transform duration-300 hover:scale-125"
         />
       ))}
     </div>
