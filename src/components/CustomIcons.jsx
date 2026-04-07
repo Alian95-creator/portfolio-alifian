9c58d9613e775bbc1a8eedb4bc13dafa4018b213
Alifian <alifiancandra93@gmail.com>
2026-04-07 21:18:30 +0700

    fix: correct svg icon paths for tech stack

--- src/components/CustomIcons.jsx
+++ src/components/CustomIcons.jsx
@@ -1,20 +1,20 @@
-import htmlIcon from "../assets/icons/html.png";
-import cssIcon from "../assets/icons/css.png";
-import jsIcon from "../assets/icons/js.png";
-import reactIcon from "../assets/icons/react.png";
-import tailwindIcon from "../assets/icons/tailwind.png";
-
 const CustomIcons = () => {
-  const icons = [htmlIcon, cssIcon, jsIcon, reactIcon, tailwindIcon];
+  const icons = [
+    "/icons/html.svg",
+    "/icons/css.svg",
+    "/icons/js.svg",
+    "/icons/react.svg",
+    "/icons/tailwind.svg",
+  ];
 
   return (
-    <div className="flex gap-4 mt-4">
+    <div className="flex gap-5 mt-5">
       {icons.map((icon, i) => (
         <img
           key={i}
           src={icon}
           alt="tech"
-          className="w-8 h-8 object-contain hover:scale-110 transition"
+          className="w-10 h-10 object-contain hover:scale-110 transition duration-300"
         />
       ))}
     </div>
