2eb6592f63dcbfe576ffd076841b4d5f2cbf890c
Alifian <alifiancandra93@gmail.com>
2026-04-07 10:02:32 +0700

    style: reduce spacing between About text and profile image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -61,7 +61,7 @@ const About = () => {
 
       {/* RIGHT IMAGE */}
       <motion.div
-        className="relative mt-10 md:mt-0 z-10 w-[280px] md:w-[320px] mx-auto"
+        className="relative mt-6 md:mt-0 z-10 w-[280px] md:w-[320px] mx-auto"
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
